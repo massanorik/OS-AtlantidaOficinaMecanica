3029481a7f54d2b8a62123b31f76fbe8e3b998ff
massanorik <celso_massanori@yahoo.com.br>
2023-04-09 23:06:01 -0300

    Adiciona arquivos do projeto

---
+++ models/DAO.java
@@ -0,0 +1,48 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class DAO.
+ */
+public class DAO {
+
+	/** The driver. */
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	
+	/** The url. */
+	private String url = "jdbc:mysql://127.0.0.1:3306/dboficina";
+	
+	/** The user. */
+	private String user = "root";
+	
+	/** The password. */
+	private String password = "123@senac";
+
+	/**
+	 * Conectar.
+	 *
+	 * @return the connection
+	 */
+	public Connection conectar() {
+
+		Connection con = null;
+
+		try {
+
+			Class.forName(driver);
+
+			con = DriverManager.getConnection(url, user, password);
+
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+
+		}
+
+	}
+
+}
